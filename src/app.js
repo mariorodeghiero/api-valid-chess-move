597b0a816b7c82b5a53d5eaffecca52637f0c4cf
Mário Antônio do Amaral Rodeghiero <mario.rodeghiero@gmail.com>
2019-01-18 23:21:07 -0200

    fix: CORS

--- src/app.js
+++ src/app.js
@@ -8,4 +8,14 @@ app.get('/calculate/:position', (req, res) => {
   res.send(result);
 });
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', ' GET, POST, PUT, DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With'
+  );
+  next();
+})
+
 app.listen(process.env.PORT || 3000, () => console.log('api running'));
